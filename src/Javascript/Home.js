6dcad2550eecc9121d425090e5916da1e427b7a4
ABHISHEK SINGH <abhishek734891@outlook.com>
2020-10-08 11:43:36 +0530

    SQL setup

--- src/Javascript/Home.js
+++ src/Javascript/Home.js
@@ -1,15 +1,14 @@
-function Validatecontactform()
-{
-    var x=document.forms["contact1"]["name"].value;
-    if(x==""){
-        alert("Name must be filled out");
-        return false;
-    }
-}
 let fname=document.getElementById('fname')
 let submit=document.getElementById('submit')
 
 submit.onclick=function (){
     let name=fname.value
-    alert('Hello'+name)
+    if(name=="")
+    {
+        alert("Name Can't Be Empty")
+    }
+    else
+    {
+        alert('Hello :'+name+". You Details have been submitted succesfully")
+    }
 }
--- src/setup.sql
+++ src/setup.sql
@@ -2,3 +2,14 @@ create database singhabhishek;
 create user singhabhishek identified with mysql_native_password by 'singhabhishek';
 grant all privileges on singhabhishek.* to singhabhishek;
 flush privileges ;
+
+use singhabhishek;
+CREATE TABLE Contact1
+(
+    Name    varchar(75),
+    Mail    varchar(75),
+    Subject varchar(75),
+    Message varchar(2000),
+    TS      TIMESTAMP DEFAULT 0 ON UPDATE CURRENT_TIMESTAMP,
+    DT      DATETIME  DEFAULT 0 ON UPDATE CURRENT_TIMESTAMP,
+);
