c4765432815f75d852dc074f12ffb8a170abd157
Abhishek Singh <abhisheksingh.nsut@gmail.com>
2021-01-28 10:26:33 +0530

    Project structure update

--- Server.js
+++ Server.js
@@ -9,13 +9,18 @@ var server=express();
 
 server.use("/contact1",PeopleRoutes)
 
-server.use('/css',express.static(path.join(__dirname, 'src/CSS')));
-server.use('/desktop',express.static(path.join(__dirname, 'src/CSS/desktop')));
+server.use('/css',express.static(path.join(__dirname, 'src/css')));
 server.use('/ico',express.static(path.join(__dirname, 'ico')));
-server.use('/Images',express.static(path.join(__dirname, 'Images')));
-server.use('/Javascript',express.static(path.join(__dirname,'src/Javascript')));
+server.use('/Images',express.static(path.join(__dirname, 'img')));
+server.use('/Javascript',express.static(path.join(__dirname,'src/javascript')));
 server.use(bodyParser.urlencoded({ extended: true }));
 
+server.get('/header',function (req,res){
+    res.sendFile('common/header.html',{root:__dirname})
+});
+server.get('/footer',function (req,res){
+    res.sendFile('common/footer.html',{root:__dirname})
+});
 server.get('/',function (req,res){
     res.sendFile('index.html',{root:__dirname})
 });
@@ -26,28 +31,28 @@ server.get('/Home',function (req,res){
     res.sendFile('index.html',{root:__dirname})
 });
 server.get('/About',function (req,res){
-    res.sendFile('src/HTML/About.html',{root:__dirname})
+    res.sendFile('src/About.html',{root:__dirname})
 });
 server.get('/Skill',function (req,res){
-    res.sendFile('src/HTML/skills.html',{root:__dirname})
+    res.sendFile('src/skills.html',{root:__dirname})
 });
 server.get('/Skill/DSA',function (req,res){
-    res.sendFile('src/skills/DSA.html',{root:__dirname})
+    res.sendFile('src/skill/DSA.html',{root:__dirname})
 });
 server.get('/Project',function (req,res){
-    res.sendFile('src/HTML/project.html',{root:__dirname})
+    res.sendFile('src/project.html',{root:__dirname})
 });
 server.get('/Contact',function (req,res){
-    res.sendFile('src/HTML/contact.html',{root:__dirname})
+    res.sendFile('src/contact.html',{root:__dirname})
 });
 server.get('/Experience',function (req,res){
-    res.sendFile('src/HTML/experience.html',{root:__dirname})
+    res.sendFile('src/experience.html',{root:__dirname})
 });
 server.get('/Gallery',function (req,res){
-    res.sendFile('src/HTML/Gallery.html',{root:__dirname})
+    res.sendFile('src/Gallery.html',{root:__dirname})
 });
 server.get('/Resume',function (req,res){
-    res.sendFile('Document/Abhishek Singh.pdf',{root:__dirname})
+    res.sendFile('docs/Abhishek Singh.pdf',{root:__dirname})
 });
 server.post('/contactform',(req, res) => {
     res.send(`${req.body.Name}':Your form has been submitted'.`);
