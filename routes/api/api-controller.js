22e9bfabe9bbd22217543fd7c62326e5d555d689
Abhishek Singh <abhisheksingh.nsut@gmail.com>
2021-04-04 09:50:02 +0530

    Added Api for fetching User Profile

--- routes/api/api-controller.js
+++ routes/api/api-controller.js
@@ -3,5 +3,6 @@ const Router = express.Router()
 const path = require('path');
 
 Router.use("/user/log", require("./userlogs"));
+Router.use("/user/profile", require("./profile"));
 
 module.exports = Router;
